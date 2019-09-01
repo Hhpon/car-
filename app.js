c2a3fe8ebd2178bbe902d2cf92ebd04b13221b63
hhp <624791164@qq.com>
2019-09-01 09:03:39 +0800

    chaneg service

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ app.post('/sendinfo', (req, res) => {
     const userInfo = req.body
     let transporter = nodemailer.createTransport({
         // host: 'smtp.ethereal.email',
-        service: 'qq', // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
+        service: '163', // 使用了内置传输发送邮件 查看支持列表：https://nodemailer.com/smtp/well-known/
         port: 465, // SMTP 端口
         secureConnection: true, // 使用了 SSL
         auth: {
-            user: '3231308323@qq.com',
+            user: 'hhpone@163.com',
             // 这里密码不是qq密码，是你设置的smtp授权码
-            pass: 'oeztywmkvoqgdbad',
+            pass: 'hyjx123',
         }
     });
 
